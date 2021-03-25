f017d66bd82dc60353f7d68cbc2af71c0a47c78a
Alina-GR <81136228+Alina-GR@users.noreply.github.com>
2021-03-25 10:58:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let B = 10;
+for(let A = 2; A < B; A++)
+{
+    if(A % 2 == 0)
+    {
+        console.log(A)
+    }
+}
+
+
+let lines = 4;
+let str = " ";
+let star = "**";
+  for(let i = 0; i < lines; i++){
+    if(i == 0){
+        console.log(" *")
+    }
+    str += star;
+    console.log(str+"*");
+}
+
+
+
+
+
